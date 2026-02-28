226b4d3fa252f5e155704f6f4d0c80e045b0482a
gio <gioele.burgarello99@gmail.com>
2026-02-28 14:58:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@
 // secondo commento da gim btw
 
 struct Cursor{
-  int y, my;
-  int x, mx;
+  int y;
+  int x;
 };
 
 struct Viewport{
@@ -27,8 +27,8 @@ struct Buffer{
 
 /***************stand*alone*func************************/
 
-void ref(const Cursor &c){
-  move(c.my, c.mx);
+void ref(const Cursor &c, const Viewport &v){
+  move((c.y - v.firstpov), c.x);
   refresh();
 }
 
@@ -39,7 +39,7 @@ void disableFlowControl(){
   tcsetattr(STDIN_FILENO, TCSANOW, &t);
 }
 
-void savefile(const Buffer &b, std::string pt){
+void savefile(const Buffer &b, const std::string &pt){
   std::ofstream file(pt);
   if(file.is_open()){
     for(int i = 0; i < b.rows.size(); i++){
@@ -59,8 +59,8 @@ void init(){
   disableFlowControl();
 }
 
-void printrow(const Cursor &c, const Buffer &b){
-  move(c.my, 0);
+void printrow(const Cursor &c, const Buffer &b, const Viewport &v){
+  move((c.y - v.firstpov), 0);
   clrtoeol();
   printw("%s", b.rows[c.y].c_str());
 }
@@ -68,13 +68,11 @@ void printrow(const Cursor &c, const Buffer &b){
 void writerow(Cursor &c, Buffer &b, int ch){
   b.rows[c.y].insert(c.x, 1, char(ch));
   c.x++;
-  c.mx++;
 }
 
 void removechar(Cursor &c, Buffer &b){
-  if(c.x > 0 && c.x <= b.rows[c.y].length() && c.y >= 0 && c.y < b.rows.size()){
+  if(c.x > 0 && c.y >= 0 && c.y < b.rows.size()){
     c.x--;
-    c.mx--;
     b.rows[c.y].erase(b.rows[c.y].begin() + c.x);
   }
 }
@@ -82,42 +80,32 @@ void removechar(Cursor &c, Buffer &b){
 void leftmove(Cursor &c){
   if(c.x - 1 >= 0){
     c.x--;
-    c.mx--;
   }
 }
 
 void rightmove(Cursor &c, const Buffer &b){
   if(c.x + 1 <= b.rows[c.y].length()){
     c.x++;
-    c.mx++;
   }
 }
 
 void upmove(Cursor &c, Viewport &v){
   if(c.y - 1 >= 0){
-    if(c.my == 0 && c.y > 0){
+    if((c.y - v.firstpov) == 0 && c.y > 0){
       v.firstpov--;
-    }else{
-      c.my--;
     }
-
     c.y--;
     c.x = 0;
-    c.mx = 0;
   }
 }
 
 void downmove(Cursor &c, const Buffer &b, Viewport &v){
   if(c.y + 1 < b.rows.size()){
-    if(c.my == LINES - 1){
+    if((c.y - v.firstpov) == LINES - 1){
       v.firstpov++; 
-    }else{
-      c.my++;
     }
-
     c.y++;
     c.x = 0;
-    c.mx = 0;
   }
 }
 
@@ -130,11 +118,23 @@ void printfile(const Viewport &v, const Buffer &b){
   }
 }
 
+void insertline(Cursor &c, Buffer &b){
+  b.rows.push_back("");
+  for(int i = b.rows.size() - 1; i > c.y; i--){
+    b.rows[i] = b.rows[i - 1];
+  }
+  
+  b.rows[c.y + 1] = b.rows[c.y].substr(c.x);
+  b.rows[c.y] = b.rows[c.y].substr(0, c.x);
+  
+  c.y++;
+  c.x = 0;
+}
+
 int main(int argc, char *argv[]){
 
   const int SAVE_KEY = 23; // ctrl+w
   const int QUIT_KEY = 24; // ctrl+x
-
   const int KEY_CTRL_LEFT = 1000; 
   const int KEY_CTRL_RIGHT = 1001;
   const int KEY_CTRL_UP = 1002; 
@@ -146,11 +146,10 @@ int main(int argc, char *argv[]){
 
   c.x = 0;
   c.y = 0;
-  c.mx = 0;
-  c.my = 0;
 
   int ch;
   int povupdate;
+
   v.firstpov = 0;
 
   std::string pt;
@@ -159,18 +158,15 @@ int main(int argc, char *argv[]){
     pt = argv[1];
     if(std::filesystem::exists(pt)){
       std::ifstream file(pt);
-
       if(file.is_open()){
         std::string line;
         while(getline(file, line)){
           b.rows.push_back(line);
         }
       }
-
       if(b.rows.empty()){
         b.rows.push_back("");
       }
-
       file.close();
     }else{
       b.rows.push_back("");
@@ -182,57 +178,58 @@ int main(int argc, char *argv[]){
 
   init();
   printfile(v, b);
-  ref(c);
-
+  ref(c, v);
 
   define_key("\033[1;5D", KEY_CTRL_LEFT);
   define_key("\033[1;5C", KEY_CTRL_RIGHT);
   define_key("\033[1;5A", KEY_CTRL_UP);
   define_key("\033[1;5B", KEY_CTRL_DOWN);
 
-
   // main loop
-
+  
   while(true){
+
     povupdate = v.firstpov;
     ch = getch();
 
     if(isprint(ch)){
       writerow(c, b, ch);
-      printrow(c, b);
-      ref(c);
+      printrow(c, b, v);
+      ref(c, v);
     }else if(ch == KEY_BACKSPACE){
       removechar(c, b);
-      printrow(c, b);
-      ref(c);
+      printrow(c, b, v);
+      ref(c, v);
     }else if(ch == SAVE_KEY){
       savefile(b, pt);
     }else if(ch == KEY_LEFT){
       leftmove(c);
-      ref(c);
+      ref(c, v);
     }else if(ch == KEY_RIGHT){
       rightmove(c, b);
-      ref(c);
+      ref(c, v);
     }else if(ch == QUIT_KEY){
       endwin();
       return 0;
     }else if(ch == KEY_UP){
       upmove(c, v);
-      ref(c);
+      ref(c, v);
     }else if(ch == KEY_DOWN){
       downmove(c, b, v);
-      ref(c);
+      ref(c, v);
+    }else if(ch == '\n'){
+      insertline(c, b);
+      printfile(v, b);
+      ref(c, v);
     }
 
     if(povupdate != v.firstpov){
       clear();
       printfile(v, b);
-      ref(c);
+      ref(c, v);
     }
 
   }
-
   endwin();
-
   return 0;
 }
